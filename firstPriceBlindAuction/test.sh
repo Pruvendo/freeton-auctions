41cb4dc1055dddccb1243c6e7e9a82a42855345b
Vadim Rusakov <darksteellord@gmail.com>
2021-06-27 16:20:39 +0300

    correct remove temp files and stop if comilation failes

--- firstPriceBlindAuction/test.sh
+++ firstPriceBlindAuction/test.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 rm contracts/*json;
 rm contracts/*tvc;
+set -e
 cd contracts/
 tondev sol compile Auction.sol;
 tondev sol compile AuctionRoot.sol;
@@ -7,6 +9,7 @@ tondev sol compile Bid.sol;
 tondev sol compile Bidder.sol;
 tondev sol compile Giver.sol;
 cd ..
+set +e
 pytest -o log_cli=true tests/;
 rm contracts/*json;
 rm contracts/*tvc;
