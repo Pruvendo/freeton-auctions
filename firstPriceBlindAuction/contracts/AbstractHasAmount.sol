fad8be512a305e345d8ee59d5c5a622a40b6b0bc
Sergey Egorov <sergey.egorov@technavology.com>
2021-07-26 14:44:50 +0300

    dir removed

--- firstPriceBlindAuction/contracts/AbstractHasAmount.sol
+++
@@ -1,9 +0,0 @@
-/* solhint-disable */
-pragma ton-solidity >= 0.35.0;
-pragma AbiHeader expire;
-pragma AbiHeader time;
-pragma AbiHeader pubkey;
-
-abstract contract AHasAmount {
-    uint128 public amount;
-}
